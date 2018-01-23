c495c28839b4473dc705fd2910604fc72890cf39
[Jessica] <[jmj5@princeton.edu]>
2018-01-23 12:27:22 -0500

    adding upload form, AJAX call

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
+#----------------------------------------------------#
+# IMPORTS
+#----------------------------------------------------#
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate 
+
+#----------------------------------------------------#
+# CONFIG
+#----------------------------------------------------#
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,29 @@
-from flask import render_template, flash, redirect, url_for
+import flask
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
+from werkzeug.utils import secure_filename
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
 
+    return render_template('index.html', title='Home')
+
+#---------------------------------------------------------------
+
+@app.route('/upload', methods = ['POST'])
+def upload():
+    if request.method == 'POST':
+    	# ADD: error handling: wrong file type (not PDF)
+        file = request.files['file']
+        if file:
+        	filename = secure_filename(file.filename)
+        	a = file.read()
+        	print a
+    return render_template('upload.html', title='Upload Result', data=a)
+
+#---------------------------------------------------------------
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -27,3 +33,7 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
