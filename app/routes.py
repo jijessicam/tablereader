c178145af1816a24e8f5fc3071f63527923dc3ae
[Jessica] <[jmj5@princeton.edu]>
2018-04-21 13:01:55 -0400

    adding helper functions for postprocessing/table coloring, part 1

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ import tabula
 import pandas as pd 
 import ast 
 import os
+import re 
 import subprocess
 import logging 
 import csv 
@@ -20,10 +21,16 @@ from io import StringIO, BytesIO
 import shutil
 import requests 
 import urllib
-from flask_wtf import FlaskForm 
+from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
 from wtforms import StringField, SubmitField, BooleanField
 from wtforms import validators, ValidationError 
+import ftfy
+
+from bokeh import plotting, models
+from bokeh.models import widgets, ColumnDataSource 
+from bokeh.models.widgets import DataTable, TableColumn
+from bokeh.embed import components 
 
 #---------------------------------------------------------------
 # APP CONFIG 
@@ -53,6 +60,14 @@ class FileUploadForm(FlaskForm):
     file_upload = FileField(validators=[FileRequired("Please select a PDF file to upload.")])
     page_range = StringField('Page Range')
     multiple_tables = BooleanField('Does your file contain multiple tables on the same page?')
+    
+    unicode_fix = BooleanField('Fix potential Unicode character encoding errors.')
+    nans_to_none = BooleanField('Replace all NaNs with None (useful for database entry)')
+    drop_duplicate_rows = BooleanField('Delete duplicate rows')
+    highlight_duplicate_rows = BooleanField('Highlight duplicate rows')
+    highlight_nans = BooleanField('Highlight missing data')
+    potential_issues = BooleanField('Highlight potential issues')
+
     submit = SubmitField('Upload File')
 
 #---------------------------------------------------------------
@@ -100,6 +115,14 @@ def upload():
         # Multiple table handling (boolean flag)
         multi_table_flag = form.multiple_tables.data
 
+        # Data postprocessing handling (more boolean flags)
+        unicode_fix = form.unicode_fix.data
+        nans_to_none = form.nans_to_none.data
+        drop_duplicate_rows = form.drop_duplicate_rows.data
+        highlight_duplicate_rows = form.highlight_duplicate_rows.data
+        highlight_nans = form.highlight_nans.data
+        potential_issues = form.potential_issues.data
+
         upload_bucket.put_object(Key=filename, Body=file.stream, ContentType=file.content_type)
         path_to_bucket = 'https://' + upload_bucket_name + '.s3.amazonaws.com/' + filename
 
@@ -107,15 +130,38 @@ def upload():
         df_list, download_name_list = tabulaParse(path_to_bucket, filename, page_range, multi_table_flag)                                                                                                                                                                                   
         
         df_html_list = []
+        bokeh_display_list = []
+
         # Handle each dataframe returned by the helper method 
         for index, df in enumerate(df_list): 
-            df_html_list.append(df.to_html()) # HTML display 
+            # df_html_list.append(df.to_html()) # HTML display 
+            # # Build Bokeh DataTable from pandas dataframe
+            # table_source = ColumnDataSource(df)
+            # bokeh_table = DataTable(source=table_source)
+            # # Add Bokeh DT to list 
+            # bokeh_display_list.append(bokeh_table)
+
+            if unicode_fix:
+                df = fixTableEncoding(df)
+            if nans_to_none:
+                df = convertNansToNone(df)
+            if drop_duplicate_rows:
+                df = df.drop_duplicates()
+
+            # Highlight problems: create boolean type map 
+            num_map = typeMapper(df)
+
+            colored_table = flagColors(df, highlight_duplicate_rows, highlight_nans, potential_issues)
+            df_html_list.append(colored_table)
+
             csv = df.to_csv(path_or_buf=None, encoding='utf-8') # will be a string 
             download_name = download_name_list[index] # match to download name 
-            download_bucket.put_object(Key=download_name, Body=csv) # put in downlad bucket
+            download_bucket.put_object(Key=download_name, Body=csv) # put in download bucket
 
-        # return render_template('upload.html', data=zip(df_html_list, download_name_list), filename=filename)
-        return render_template('upload.html', tables=zip(df_html_list, download_name_list), dnames=download_name_list, filename=filename)
+        # script, div = components(bokeh_display_list)
+
+        return render_template('upload.html', tables=zip(div, download_name_list), dnames=download_name_list, filename=filename)
+        # return render_template('upload.html', tables=zip(df_html_list, download_name_list), dnames=download_name_list, filename=filename)
 
     # Error handling: form not validated 
     else:
@@ -156,6 +202,158 @@ def tabulaParse(file_contents, filename, pages, multitable):
 
 #---------------------------------------------------------------
 
+# Postprocessing: drop duplicate rows if boolean flag is True
+def dropDuplicateRows(input_dataframe):
+    df = input_dataframe
+    df = df.drop_duplicates()
+    return df 
+
+#---------------------------------------------------------------
+
+# Postprocessing: convert all NaN values to None 
+def convertNansToNone(input_dataframe):
+    df = input_dataframe
+    df = df.where((pd.notnull(df)), None)
+    return df 
+
+#---------------------------------------------------------------
+
+# Postprocessing: fix Unicode encoding if boolean flag is True 
+def fixTableEncoding(input_dataframe):
+    df = input_dataframe
+    df = df.applymap(fixTextEncoding)
+    return df 
+
+def fixTextEncoding(text):
+    if isinstance(text, basestring):
+        t = bytes.decode(text) # decode to utf-8
+        return ftfy.fix_text(t) # ftfy library call
+    else:
+        return 
+
+#---------------------------------------------------------------
+
+# Helper method: dataframe coloring 
+# Takes dataframe, boolean map as input 
+def flagColors(input_dataframe, highlight_duplicates, highlight_nans, potential_issues):
+    df = input_dataframe
+    RED = '#fc5d58'
+
+    # RED -----------------------------
+    # 1) flag all NaNs
+    # df = df.style.highlight_null(null_color=RED)
+    # df = df.style.set_table_attributes('border=1')
+
+    df = df.style. \
+        highlight_null(null_color=RED). \
+        set_table_attributes('border=1')
+
+    return df.render()
+
+#---------------------------------------------------------------
+
+def highlightDuplicateRows(input_dataframe):
+
+    df = input_dataframe 
+    d = df.duplicated()
+    dupe_rows = [i for i, x in enumerate(d) if x] # list of dupe indices
+    for index in dupe_rows:
+        df.iloc[[index]] = 'background-color: yellow'
+    return df 
+
+#---------------------------------------------------------------
+
+# Helper method: return a list of dataframe row indices that could be headers 
+def identifyHeaders(boolean_map):
+    df = boolean_map 
+    num_rows = df.shape[0]
+    num_cols = df.shape[1]
+
+    last_row = df.iloc[[num_rows-1]].reset_index(drop=True)
+
+    mismatch_index = -1 
+
+    for index, row in df.iterrows():
+        if (last_row.equals(df.iloc[[index]].reset_index(drop=True))):
+            mismatch_index = index
+            break 
+        else:
+            continue 
+
+    if mismatch_index > 0:
+        header_list = range(1, mismatch_index)
+    else: 
+        header_list = []
+        return header_list, df 
+
+    df = df.drop(df.index[header_list])
+    df = df.drop(df.index[0])
+    return header_list, df 
+
+#---------------------------------------------------------------
+
+def colorNonUniqueColumnValues(cell_dict):
+    d = cell_dict 
+
+    for key, index_list in d.iteritems():
+        for index in index_list: 
+            
+
+#---------------------------------------------------------------
+
+# Helper method: returns a dict of {col name, [list of row indices]} to flag irregular data 
+def nonUniqueColumnValues(boolean_map):
+    df = boolean_map
+
+    uniques = df.apply(lambda x: x.nunique())
+
+    df = df.drop(uniques[uniques==1].index, axis=1)
+
+    val_counts = df.apply(pd.value_counts)
+
+    flag_dict = {}
+
+    for col_name, column in df.iteritems():
+        if val_counts.iloc[0][col_name] < val_counts.iloc[1][col_name]:
+            index_list = df.index[df[col_name] == val_counts.iloc[0].name].tolist()
+            flag_dict[col_name] = index_list
+        else: 
+            index_list = df.index[df[col_name] == val_counts.iloc[1].name].tolist()
+            flag_dict[col_name] = index_list
+
+    return flag_dict 
+
+#---------------------------------------------------------------
+
+# Helper method: regex mapping 
+
+def typeMapper(input_dataframe):
+    df = input_dataframe 
+    df = df.applymap(regexMapHelper)
+    return df 
+
+def regexMapHelper(obj):
+
+    num = re.compile(r"^[+-]?\d+(?:(\.|,)\d+)?$")
+
+    # Matches numbers in this format: 1,234,567 
+    num2 = re.compile(r"^((?:\d{1,3},(?:\d{3},)*\d{3})|(?:\d{1,3}))$")
+
+    # Modification to accomodate commas and decimals 
+    num3 = re.compile(r"^[+-]?[\d,]+(?:(\.|,)\d+)?$")
+
+    cell = str(obj) # convert to string 
+
+    if num.match(cell):
+        return True 
+    elif num2.match(cell):
+        return True 
+    elif num3.match(cell):
+        return True 
+    else: 
+        return False
+#---------------------------------------------------------------
+
 @app.route('/download', methods = ['GET', 'POST'])
 def download():
 
@@ -187,69 +385,3 @@ def download():
         memory_file.seek(0)
 
         return send_file(memory_file, attachment_filename="output.zip", as_attachment=True)
-
-#----------------------------------------------------
-
-# @app.route('/test', methods = ['GET'])
-# def test():
-#   return render_template('upload_form.html', landing_page = 'process')
-
-#----------------------------------------------------
-
-# @app.route('/process', methods = ['GET','POST'])
-# def process():
-#   if request.method == 'POST':
-#     file = request.files['file']
-#     hocr = request.form.get('hocr') or ''
-#     ext = '.hocr' if hocr else '.txt'
-#     if file and allowed_file(file.filename):
-#       folder = os.path.join(app.config['TEMP_FOLDER'], str(os.getpid()))
-
-#       os.mkdir(folder)
-#       input_file = os.path.join(folder, secure_filename(file.filename))
-#       output_file = os.path.join(folder, app.config['OCR_OUTPUT_FILE'])
-#       file.save(input_file)
-
-#       print "input file check: ", os.path.isfile(input_file)
-#       print "input file path: ", input_file 
-#       print "output file path: ", output_file 
-
-#       command = ['tesseract', input_file, output_file, '-l', request.form['lang'], hocr]
-#       proc = subprocess.Popen(command, stderr=subprocess.PIPE)
-#       proc.wait()
-
-#       output_file += ext
-
-#       print output_file 
-#       print "output file exists check: ", os.path.exists(output_file)
-#       print "output file is file check: ", os.path.isfile(output_file)
-
-#       if os.path.isfile(output_file):
-#         f = open(output_file)
-#         resp = jsonify( {
-#           u'status': 200,
-#           u'ocr':{k:v.decode('utf-8') for k,v in enumerate(f.read().splitlines())}
-#         } )
-#       else:
-#         resp = jsonify( {
-#           u'status': 422,
-#           u'message': u'Unprocessable Entity'
-#         } )
-#         resp.status_code = 422
-
-#       shutil.rmtree(folder)
-#       return resp
-#     else:
-#       resp = jsonify( { 
-#         u'status': 415,
-#         u'message': u'Unsupported Media Type' 
-#       } )
-#       resp.status_code = 415
-#       return resp
-#   else:
-#     resp = jsonify( { 
-#       u'status': 405, 
-#       u'message': u'The method is not allowed for the requested URL' 
-#     } )
-#     resp.status_code = 405
-#     return resp
